d16691793e04836ed3590317ce3c20b612bda10a
Vinicius de Oliveira <vinicius.oliveira@sequor.com.br>
2021-12-28 22:38:04 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header";
+import Header from "./components/header/index";
 import Salas from "./pages/salas";
 import {
   BrowserRouter as Router,
