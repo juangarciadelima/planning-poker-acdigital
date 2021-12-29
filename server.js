77a4eae440b723ed80627f44b708a43e6dacdedb
Vinicius de Oliveira <vinicius.oliveira@sequor.com.br>
2021-12-29 00:13:37 -0300

    teste

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const path = require('path');
 const express = require('express');
+const { resolve } = require('path');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
+
+app.use(express.static(
+  resolve(
+    __dirname,
+    './build'
+  )
+));
+
+app.listen(process.env.PORT || 3000, (err) => {
+   if(err)
+      console.log('Error ==> ', err);
    console.log('Server is up!');
 });
